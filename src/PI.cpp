d70a0b74a43eb57b951d2e493c323d8457d86dc7
Aleksei Luchinskii <aluchi@bgsu.edu>
2022-07-30 08:38:24 -0400

    PI.cpp

--- src/PI.cpp
+++ src/PI.cpp
@@ -65,51 +65,51 @@ NumericVector computePI(NumericMatrix D,int homDim,
                            int res, double sigma,
                            double minB, double maxB,
                            double minP, double maxP){
-  // D - N by 3 matrix (columns contain dimension, birth and persistence values respectively)
-  int n_rows = 0; // number of rows with the correct dimension
+// D - N by 3 matrix (columns contain dimension, birth and persistence values respectively)
+int n_rows = 0; // number of rows with the correct dimension
   for(int i=0; i<D.nrow(); ++i) {
     if(D(i,0) == homDim) {
       ++n_rows; 
     }
   }
   
-  if (n_rows == 0) return NumericVector(pow(res,2));
-  
-  NumericMatrix D_(n_rows,2);
-  int j=0;
-  for(int i=0; i<D.nrow(); ++i) {
-    if(D(i,0) == homDim) {
-      D_(j,0) = D(i,1);
-      D_(j,1) = D(i,2);
-      ++j;
-    }
+if (n_rows == 0) return NumericVector(pow(res,2));
+
+NumericMatrix D_(n_rows,2);
+int j=0;
+for(int i=0; i<D.nrow(); ++i) {
+  if(D(i,0) == homDim) {
+    D_(j,0) = D(i,1);
+    D_(j,1) = D(i,2);
+    ++j;
   }
-  
-  double dy = (maxP-minP)/res;
-  NumericVector y_lower = seq_C(minP,maxP-dy,dy);
-  NumericVector y_upper = y_lower + dy;
-  int Nsize;
-  double sumB = sum(abs(diff(D_(_,0))));
-  if ((homDim==0)&(sumB==0)) {
-    Nsize = res;
+}
+
+double dy = (maxP-minP)/res;
+NumericVector y_lower = seq_C(minP,maxP-dy,dy);
+NumericVector y_upper = y_lower + dy;
+int Nsize;
+double sumB = sum(abs(diff(D_(_,0))));
+if ((homDim==0)&(sumB==0)) {
+  Nsize = res;
   }else{
-    Nsize = pow(res,2);
-  }
-  NumericMatrix Psurf_mat(Nsize,n_rows);
+  Nsize = pow(res,2);
+}
+NumericMatrix Psurf_mat(Nsize,n_rows);
   
-  if (Nsize==res) {
-    for(int i=0; i<n_rows; ++i){
-      Psurf_mat(_,i) = PSurfaceH0(D_(i,_),y_lower,y_upper,sigma,maxP);
-    }
-    
-  } else{
-    double dx = (maxB-minB)/res;
-    NumericVector x_lower = seq_C(minB,maxB-dx,dx);
-    NumericVector x_upper = x_lower + dx;
-    for(int i=0; i<n_rows; ++i){
-      Psurf_mat(_,i) = PSurfaceHk(D_(i,_),y_lower,y_upper,x_lower,x_upper,sigma,maxP);
-    }
+if (Nsize==res) {
+  for(int i=0; i<n_rows; ++i){
+    Psurf_mat(_,i) = PSurfaceH0(D_(i,_),y_lower,y_upper,sigma,maxP);
   }
-  NumericVector out = rowSums_C(Psurf_mat);
-  return out;
+
+} else{
+  double dx = (maxB-minB)/res;
+  NumericVector x_lower = seq_C(minB,maxB-dx,dx);
+  NumericVector x_upper = x_lower + dx;
+  for(int i=0; i<n_rows; ++i){
+    Psurf_mat(_,i) = PSurfaceHk(D_(i,_),y_lower,y_upper,x_lower,x_upper,sigma,maxP);
+  }
+}
+NumericVector out = rowSums_C(Psurf_mat);
+return out;
 }
