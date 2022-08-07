1d811944ee3c1369bddcec62425060dca695bdad
TDAvec <75586940+TDAvecs@users.noreply.github.com>
2022-08-07 19:26:02 -0400

    Add files via upload

--- src/ECC.cpp
+++ src/ECC.cpp
@@ -2,35 +2,7 @@
 #include <iostream>
 using namespace Rcpp;
 
-NumericVector computeVAB(NumericMatrix D, int homDim, NumericVector scaleSeq){
-  int n_rows = 0; // number of rows with the correct dimension
-  for(int i=0;i<D.nrow();++i){
-    if(D(i,0) == homDim){
-      ++n_rows; 
-    }
-  }
-  
-  if (n_rows == 0) return NumericVector(scaleSeq.size()-1);
-  
-  NumericVector x(n_rows),y(n_rows);
-  int n=0;
-  for(int i=0;i<D.nrow();++i){
-    if( D(i,0) == homDim){
-      x[n] = D(i,1);
-      y[n] = D(i,2);
-      ++n;
-    }
-  }
-  
-  int l = scaleSeq.size()-1; 
-  NumericVector vab(l);
-  NumericVector b(n);
-  for (int k=0;k<l;++k){
-    b = pmin(scaleSeq[k+1],y)-pmax(scaleSeq[k],x);
-    vab[k] = sum(pmax(0,b))/(scaleSeq[k+1]-scaleSeq[k]);
-  }
-  return vab; 
-}
+NumericVector computeVAB(NumericMatrix D, int homDim, NumericVector scaleSeq);
 
 // [[Rcpp::export]]
 NumericVector computeECC(NumericMatrix D, int maxhomDim, NumericVector scaleSeq){
